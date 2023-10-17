d7bdfa7cac96027950b441fc98bfa5b3a42a4ffd
ahmedsamir-cs50 <ahmed.samir2051996@gmail.com>
2023-10-17 11:35:09 +0300

    k

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+import { PadBankData } from "./padBankData";
 
-function App() {
+const App = () => {
+  const [volume, setVolume] = useState(1);
+  const [disableBtn, setDisableBtn] = useState(true);
+  const [screenValue, setScreenValue] = useState("");
+
+  // Handle volume change
+  const handleVolume = (e) => {
+    setVolume(e.target.value);
+  };
+
+  // Handle power button click
+  const handlePower = () => {
+    setDisableBtn(!disableBtn)
+    console.log(disableBtn)
+    setScreenValue("");
+    document.getElementById("switch").style.cssFloat = disableBtn
+      ? "right"
+      : "left";
+    document.getElementById("switch").style.background = disableBtn
+      ? "green"
+      : "#92140c";
+  };
+
+  // Render the component
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="inner-container">
+      <div className="pad-bank">
+        {/* Render AudioButton component for each pad */}
+        
+        {
+        !disableBtn ?
+        (PadBankData.map((pad) => {
+          return (
+            <AudioButton
+              key={pad.no}
+              pad={pad}
+              disableBtn={disableBtn}
+              volume={volume}
+              setScreenValue={setScreenValue}
+            />
+          );
+        }
+        )):<h3 className="m-auto text-center">Turn Power On</h3>
+        }
+      </div>
+      <div className="controls-container">
+        <div className="Power-wrapper">
+          <p>power</p>
+          <div className="switch-wrapper" onClick={handlePower}>
+            <div className="switch" id="switch"></div>
+          </div>
+        </div>
+        <div className="screen">{screenValue}</div>
+        <div className="range-wrapper">
+          <h5 className="text-center">Volume</h5>
+          <input
+            type="range"
+            min="0"
+            step="0.1"
+            max="1"
+            onChange={handleVolume}
+            value={volume}
+          ></input>
+        </div>
+      </div>
     </div>
   );
-}
 
+
+// AudioButton component
+
+function AudioButton({ pad, disableBtn, volume, setScreenValue }) {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  
+  }, []);
+
+  // Handle key press event
+  const handleKeyPress = (event) => {
+    if (event.key === pad.id_audio.toLowerCase() ) {
+      playSound();
+    }
+  };
+
+  // Play the sound
+  const playSound = () => {
+    const audioTag = document.getElementById(pad.id_audio);
+    console.log(disableBtn)
+     
+   
+      setScreenValue(pad.id_main);
+      audioTag.currentTime = 0;
+      audioTag.play();
+      setActive(true);
+      setTimeout(() => {
+        setActive(false);
+      }, 200);
+  
+    audioTag.volume = volume;
+  };
+
+  // Render the component
+  return (
+    <button
+      disabled={!disableBtn}
+      className={!active ? "drum-pad" : "drum-active"}
+      id={pad.id_main}
+      onClick={playSound}
+    >
+      <audio src={pad.source} id={pad.id_audio}></audio>
+      {pad.id_audio}
+    </button>
+  );
+}
+}
 export default App;
---
+++ src/padBankData.js
@@ -0,0 +1,66 @@
+import source1 from './assets/A.mp3'
+import source2 from './assets/C.mp3'
+import source3 from './assets/D.mp3'
+import source4 from './assets/E.mp3'
+import source5 from './assets/Q.mp3'
+import source6 from './assets/S.mp3'
+import source7 from'./assets/W.mp3'
+import source8 from './assets/X.mp3'
+import source9 from './assets/Z.mp3'
+
+export const PadBankData =[
+    {
+        no:1,
+    id_main:"Heater-1",
+    id_audio:"Q",
+    source:source1,
+    },
+    {
+        no:2,
+    id_main:"Heater-2",
+    id_audio:"W",
+    source:source2
+    },
+    {
+        no:3,
+    id_main:"Heater-3",
+    id_audio:"E",
+    source:source3,
+    },
+    {
+        no:4,
+    id_main:"Heater-4",
+    id_audio:"A",
+    source:source4,
+    },
+    {
+        no:5,
+    id_main:"Clap",
+    id_audio:"S",
+    source:source5,
+    },
+    {
+        no:6,
+    id_main:"Open-HH",
+    id_audio:"D",
+    source:source6,
+    },
+    {
+        no:7,
+    id_main:"Kick-n'-Hat",
+    id_audio:"Z",
+    source:source7
+    },
+    {
+        no:8,
+    id_main:"Kick",
+    id_audio:"X",
+    source:source8
+    },
+    {
+        no:9,
+    id_main:"Closed-HH",
+    id_audio:"C",
+    source:source9,
+    }
+]
